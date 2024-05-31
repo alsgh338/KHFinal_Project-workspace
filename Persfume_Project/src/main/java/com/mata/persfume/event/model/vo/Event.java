09e2bd4181ba0fa8bbe23ecdde7a142911750c4f
alsgh338 <alsgh338@gmail.com>
2024-05-31 18:05:37 +0900

    Event VO, mapper 추가 - 민호
    EVENT 관련 VO 생성
    EVENT 관련 mapper 생성
    mybatis-config에 event 관련 추가

--- Persfume_Project/src/main/java/com/mata/persfume/event/model/vo/Event.java
+++ Persfume_Project/src/main/java/com/mata/persfume/event/model/vo/Event.java
@@ -1,5 +1,24 @@
 package com.mata.persfume.event.model.vo;
 
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Event {
 
+	private int eventNo;
+	private String eventTitle;
+	private String eventContent;
+	private String eventCount;
+	private Date startDate;
+	private Date dewDate;
+	private Date createDate;
+	private String status;
 }
