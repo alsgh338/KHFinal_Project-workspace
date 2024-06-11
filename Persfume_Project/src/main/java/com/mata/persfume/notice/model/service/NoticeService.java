4aa210d2cd1c273e43a5b2a2f5fce66868117f3a
ag_seo <kes1194@naver.com>
2024-06-11 12:33:14 +0900

    Update NoticeService.java
    feat - NoticeService

--- Persfume_Project/src/main/java/com/mata/persfume/notice/model/service/NoticeService.java
+++ Persfume_Project/src/main/java/com/mata/persfume/notice/model/service/NoticeService.java
@@ -1,5 +1,46 @@
 package com.mata.persfume.notice.model.service;
 
-public interface NoticeService {
+import java.util.ArrayList;
 
+import com.mata.persfume.common.model.vo.PageInfo;
+import com.mata.persfume.notice.model.vo.Notice;
+
+
+
+
+//import com.kh.spring.common.model.vo.PageInfo;
+
+public  interface NoticeService {
+
+
+
+	// 공지글  리스트 조회 서비스 + 페이징 처리
+	// 공지글  총 갯수 조회
+	int selectListCount();
+	
+	// 공지글  리스트 조회
+	ArrayList<Notice> selectList(PageInfo pi);
+	
+	// 공지글 작성하기 서비스
+	int insertNotice(Notice n);
+
+
+	// 공지글  조회수 증가
+	int increaseCount(int noticeNo);
+	// 공지글  상세조회 서비스
+	Notice selectNotice(int noticeNo);
+	
+	// 공지글 삭제 서비스
+	int deleteNotice(int noticeNo);
+	
+	// 공지글 수정 서비스
+	int updateNotice(Notice n);
+	
+	
 }
+
+
+
+
+
+
